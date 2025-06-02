f1e8791f44f4ce3eb3dd01bb4f16934925b0685e
Nicole352 <nicole352diaz@gmail.com>
2025-06-01 21:48:02 -0500

    Aplicación de Funcionalidades
    . Usuarios conectados
    . Notificación de usuario que se une o abandona el chat

--- src/realTimeServer.js
+++ src/realTimeServer.js
@@ -1,15 +1,44 @@
 module.exports = (httpServer) => {
   const { Server } = require("socket.io");
   const io = new Server(httpServer);
+  const connectedUsers = new Map();
+
   io.on("connection", (socket) => {
     const cookie = socket.handshake.headers.cookie;
-    const user = cookie.split("=").pop();
+    if (!cookie) return;
+
+    const userCookie = cookie
+      .split(";")
+      .map((c) => c.trim())
+      .find((c) => c.startsWith("username="));
+    if (!userCookie) return;
+
+    const username = userCookie.split("=")[1];
+    connectedUsers.set(socket.id, username);
+
+    // Enviar mensaje de unión
+    io.emit("message", {
+      user: "🔔",
+      message: `${username} se ha unido al chat.`,
+    });
+
+    // Enviar lista actualizada
+    io.emit("users-list", Array.from(connectedUsers.values()));
 
     socket.on("message", (message) => {
       io.emit("message", {
-        user,
+        user: username,
         message,
       });
+    });
+
+    socket.on("disconnect", () => {
+      connectedUsers.delete(socket.id);
+      io.emit("message", {
+        user: "🔔",
+        message: `${username} salió del chat.`,
+      });
+      io.emit("users-list", Array.from(connectedUsers.values()));
     });
   });
 };
